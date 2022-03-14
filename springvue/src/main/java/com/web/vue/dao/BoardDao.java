673bda175011d86d85f6ca139e7c6b5720724b66
yujinseon <rjh6708@naver.com>
2022-03-14 10:15:57 +0900

    0314

--- springvue/src/main/java/com/web/vue/dao/BoardDao.java
+++ springvue/src/main/java/com/web/vue/dao/BoardDao.java
@@ -1,9 +1,15 @@
 package com.web.vue.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.web.vue.vo.Board;
+import com.web.vue.vo.BoardSch;
 
 // com.web.vue.dao.BoardDao
 @Mapper
 public interface BoardDao {
-
+	public int getTot(BoardSch sch);
+	public List<Board> getList(BoardSch sch);
 }
